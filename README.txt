bd8f80c5b5ca4f3e99bddc95166300dbf6a7ac8c
Andrew Knickman <andrewsknick@gmail.com>
2018-12-07 00:12:34 -0500

    Final Pyrush Project Submission

---
+++ README.txt
@@ -0,0 +1,5 @@
+PYRUSH Shell
+COSC 439 Project
+Built By: Andrew Knickman
+Pyrush Team: Andrew Knickman, Niyi Ajayi
+Uses Papyrus syntax to pass commands to system
--- pyrush.c
+++ pyrush.c
@@ -1,6 +1,7 @@
 /*
 PYRUSH Shell
 COSC 439 Project
+Written By: Andrew Knickman
 Pyrush Team: Andrew Knickman, Niyi Ajayi
 
 Uses Papyrus syntax to pass commands to system
